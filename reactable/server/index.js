16daa4284fd1e8e6b2d28d52e9b4b07fa4a40903
Avinash Arora <aviarora@publicisgroupe.net>
2019-03-07 16:03:50 +0530

    remove create react app and added react boiler plate

--- reactable/server/index.js
+++ reactable/server/index.js
@@ -1,90 +1,56 @@
-#!/usr/bin/env node
+/* eslint consistent-return:0 import/order:0 */
 
-/**
- * Module dependencies.
- */
+const express = require('express');
+const logger = require('./logger');
 
-var app = require('./app');
-var debug = require('debug')('server:server');
-var http = require('http');
+const argv = require('./argv');
+const port = require('./port');
+const setup = require('./middlewares/frontendMiddleware');
+const isDev = process.env.NODE_ENV !== 'production';
+const ngrok =
+  (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel
+    ? require('ngrok')
+    : false;
+const { resolve } = require('path');
+const app = express();
 
-/**
- * Get port from environment and store in Express.
- */
+// If you need a backend, e.g. an API, add your custom backend-specific middleware here
+// app.use('/api', myApi);
 
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+// In production we need to pass these values in instead of relying on webpack
+setup(app, {
+  outputPath: resolve(process.cwd(), 'build'),
+  publicPath: '/',
+});
 
-/**
- * Create HTTP server.
- */
+// get the intended host and port number, use localhost and port 3000 if not provided
+const customHost = argv.host || process.env.HOST;
+const host = customHost || null; // Let http.Server use its default IPv6/4 host
+const prettyHost = customHost || 'localhost';
 
-var server = http.createServer(app);
+// use the gzipped bundle
+app.get('*.js', (req, res, next) => {
+  req.url = req.url + '.gz'; // eslint-disable-line
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
 
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
+// Start your app.
+app.listen(port, host, async err => {
+  if (err) {
+    return logger.error(err.message);
   }
 
-  if (port >= 0) {
-    // port number
-    return port;
+  // Connect to ngrok in dev mode
+  if (ngrok) {
+    let url;
+    try {
+      url = await ngrok.connect(port);
+    } catch (e) {
+      return logger.error(e);
+    }
+    logger.appStarted(port, prettyHost, url);
+  } else {
+    logger.appStarted(port, prettyHost);
   }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+});
--- reactable/server/routes/invoice.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const invoiceController = require('./invoiceController');
-
-router
-    .route('/')
-    .get(invoiceController.get)
-
-module.exports = router;
