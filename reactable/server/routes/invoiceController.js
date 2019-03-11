a71c8e5cb9486323fbbcbada7a706b88dd6bc1e7
Avinash Arora <aviarora@publicisgroupe.net>
2019-03-11 12:31:25 +0530

    fixed params casing

--- reactable/server/routes/invoiceController.js
+++ reactable/server/routes/invoiceController.js
@@ -1,40 +1,44 @@
-const Invoice = require("../model/InvoiceModel");
+const Invoice = require('../model/InvoiceModel');
 
 const filter = {
-  ALL : "all",
-  STATUS: "status",
-}
+  ALL: 'all',
+  STATUS: 'status',
+};
 
 // order : asc | desc
 
 exports.get = (req, res) => {
-  const { startIndex = 0, itemPerPage = 2, orderby, order = 'asc' } = req.query;
-  Invoice.count( {}, (err, count) => {
+  const {
+    startindex = 0,
+    itemsperpage = 2,
+    orderby,
+    order = 'asc',
+  } = req.query;
+  Invoice.count({}, (err, count) => {
     if (err) {
       res.status(500).send(err);
     }
     Invoice.find({})
-      .sort( { [orderby] : order } )
-      .skip(parseInt(startIndex))
-      .limit(parseInt(itemPerPage))
-      .exec((err, invoice) => {
-        if (err) {
-          res.status(500).send(err);
+      .sort({ [orderby]: order })
+      .skip(parseInt(startindex, 10))
+      .limit(parseInt(itemsperpage, 10))
+      .exec((error, invoice) => {
+        if (error) {
+          res.status(500).send(error);
         }
         const metaData = {
           count,
-          startIndex,
-          itemPerPage,
-        }
+          startindex,
+          itemsperpage,
+        };
         if (orderby) {
           metaData.orderby = orderby;
           metaData.order = order;
         }
-        res.status(200)
-          .json({
-            data : invoice,
-            metaData
-          });
+        res.status(200).json({
+          data: invoice,
+          metaData,
+        });
       });
-  })
+  });
 };
