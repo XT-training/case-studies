b6e8967941ce48d4e4f250a3e089cfbb0fbd18ea
Sunil Srivastava <ssrivastava66@sapient.com>
2019-03-16 15:31:47 +0530

    quickview integrated

--- reactable/src/components/Reactable/Reactable.jsx
+++ reactable/src/components/Reactable/Reactable.jsx
@@ -1,36 +1,60 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from '@emotion/styled';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
 
-import Table from '../Table/Table';
-import Thead from '../Thead/Thead';
-import Tbody from '../Tbody/Tbody';
-import Tr from '../Tr/Tr';
-import Th from '../Th/Th';
-import Td from '../Td/Td';
+import Table from "../Table/Table";
+import Thead from "../Thead/Thead";
+import Tbody from "../Tbody/Tbody";
+import Tr from "../Tr/Tr";
+import Th from "../Th/Th";
+import Td from "../Td/Td";
 
-const Head = ({columns, cellDensity}) => (<Thead>
-<Tr>
-  {columns.map(headingObject => <Th cellDensity={cellDensity} key={headingObject.key}>{headingObject.value}</Th>)}
-</Tr>
-</Thead>);
+const Head = ({ columns, cellDensity }) => (
+  <Thead>
+    <Tr>
+      {columns.map(headingObject => (
+        <Th cellDensity={cellDensity} key={headingObject.key}>
+          {headingObject.value}
+        </Th>
+      ))}
+    </Tr>
+  </Thead>
+);
 
-const Body = ({data, columns, cellDensity}) => (<Tbody>
-{data.map((row, index) => {
-  return <Tr key={`row_${index}`}>
-    {columns.map(headingObject => {
-      const isValueObject = typeof row[headingObject.key] === 'object';
-      const isTh = isValueObject && row[headingObject.key].type === 'heading';
-      const Component = isTh ? Th : Td;
-      const otherProp = isTh ? {
-        heading: true,
-      } : {};
-      return <Component cellDensity={cellDensity} key={`col_${headingObject.key}`} {...otherProp}>{isValueObject ? row[headingObject.key].value : row[headingObject.key]}</Component>;
+const Body = ({ data, columns, cellDensity }) => (
+  <Tbody>
+    {data.map((row, index) => {
+      return (
+        <Tr key={`row_${index}`}>
+          {columns.map(headingObject => {
+            const isValueObject =
+              typeof row[headingObject.key] === "object" &&
+              row[headingObject.key].value;
+            const isTh =
+              isValueObject && row[headingObject.key].type === "heading";
+            const Component = isTh ? Th : Td;
+            const otherProp = isTh
+              ? {
+                  heading: true
+                }
+              : {};
+            return (
+              <Component
+                cellDensity={cellDensity}
+                key={`col_${headingObject.key}`}
+                {...otherProp}
+              >
+                {isValueObject
+                  ? row[headingObject.key].value
+                  : row[headingObject.key]}
+              </Component>
+            );
+          })}
+        </Tr>
+      );
     })}
-  </Tr>
-})}
-</Tbody>)
-
+  </Tbody>
+);
 
 class Reactable extends React.PureComponent {
   static propTypes = {
@@ -43,30 +67,33 @@ class Reactable extends React.PureComponent {
     super(props);
     this.tableRef = React.createRef();
     this.state = {
-      styles: {},
-    }
+      styles: {}
+    };
   }
 
   componentDidMount() {
-      const tableCont = this.tableRef.current;
-      if(this.tableCont){
-        this.setState({
-          styles: {width: tableCont.clientWidth, height: tableCont.clientHeight}
-        });
-      }
-
+    const tableCont = this.tableRef.current;
+    if (this.tableCont) {
+      this.setState({
+        styles: { width: tableCont.clientWidth, height: tableCont.clientHeight }
+      });
+    }
   }
 
-  render(){
+  render() {
     const { data, className, columns, cellDensity } = this.props;
-    if(data instanceof Array && data.length > 0){
+    if (data instanceof Array && data.length > 0) {
       return (
-      <div className={className} style={this.state.styles} ref={this.tableRef}>
-        <Table>
-          <Head columns={columns} cellDensity={cellDensity} />
-          <Body data={data} columns={columns} cellDensity={cellDensity} />
+        <div
+          className={className}
+          style={this.state.styles}
+          ref={this.tableRef}
+        >
+          <Table>
+            <Head columns={columns} cellDensity={cellDensity} />
+            <Body data={data} columns={columns} cellDensity={cellDensity} />
           </Table>
-      </div>
+        </div>
       );
     }
 
@@ -76,8 +103,8 @@ class Reactable extends React.PureComponent {
 
 Reactable.defaultProps = {
   data: [],
-  cellDensity: 0.5,
-}
+  cellDensity: 0.5
+};
 
 export default styled(Reactable)`
   font-size: 1rem;
