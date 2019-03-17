eaf472442b201a49d4e60d174b023d454125e138
Satyendera Sharma 2 <ssharma191@sapient.com>
2019-03-17 22:34:29 +0530

    updated

--- _SSR/app/pages/error-page/ErroPage.test.js
+++ _SSR/app/pages/error-page/ErroPage.test.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ErrorPage from './ErrorPage';
 
 describe('ErrorPage', () => {
-    it('should render correctly in "debug" mode', () => {
+    it('Error Page > should render correctly in "debug" mode', () => {
         const component = shallow(<ErrorPage debug />);
 
         expect(component).toMatchSnapshot();
